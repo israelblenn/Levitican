d52a8dd01c0ea565d663c75f138a7c915726fdd1
Israel <israelblenn@gmail.com>
2024-12-06 17:23:55 +1100

    new social icons and mobile nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,13 @@ import CloseNav from "@/src/components/closenav";
 import logo from "@/public/logo.svg";
 import youtube from "@/public/youtube.svg";
 import instagram from "@/public/instagram.svg";
-import twitter from "@/public/twitter.svg";
+import x from "@/public/x.svg";
 
 export const metadata: Metadata = {
   title: "Levitican",
 };
 
 export default function RootLayout({ children, }: Readonly<{children: React.ReactNode; }>) {
-
   return (
     <html lang="en">
       <body>
@@ -21,9 +20,9 @@ export default function RootLayout({ children, }: Readonly<{children: React.Reac
           <div className="nav-header">
             <Link href="/"><Image src={logo} alt="levitican logo" height={72} className="logo" priority /></Link>
             <div className="socials">
-              <Link className="flex" href={'https://www.youtube.com/@LEVITICAN'}><Image src={youtube} alt="youtube" height={21}/></Link>
+              <Link className="flex" href={'https://www.youtube.com/@LEVITICAN'}><Image src={youtube} alt="youtube" height={21} width={24}/></Link>
               <Link className="flex" href={'https://instagram.com/LEVITICAN_ARTS'}><Image src={instagram} alt="instagram" height={21}/></Link>
-              <Link className="flex" href={'https://x.com/LEVITICAN_ARTS'}><Image src={twitter} alt="twitter" height={21}/></Link>
+              <Link className="flex" href={'https://x.com/LEVITICAN_ARTS'}><Image src={x} alt="x" height={21}/></Link>
             </div>
           </div>
           <label className="hamburger"><input type="checkbox" /></label>
@@ -36,9 +35,9 @@ export default function RootLayout({ children, }: Readonly<{children: React.Reac
               <li><Link href="/contact">CONTACT</Link></li>
             </ul>
             <div className="socials-mobile">
-              <Link className="flex" href={'https://www.youtube.com/@LEVITICAN'}><Image src={youtube} alt="youtube" height={26}/></Link>
+              <Link className="flex" href={'https://www.youtube.com/@LEVITICAN'}><Image src={youtube} alt="youtube" height={26} width={30}/></Link>
               <Link className="flex" href={'https://instagram.com/LEVITICAN_ARTS'}><Image src={instagram} alt="instagram" height={26}/></Link>
-              <Link className="flex" href={'https://x.com/LEVITICAN_ARTS'}><Image src={twitter} alt="twitter" height={26}/></Link>
+              <Link className="flex" href={'https://x.com/LEVITICAN_ARTS'}><Image src={x} alt="x" height={26}/></Link>
             </div>
           </nav>
         </header>
