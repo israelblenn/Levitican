e5f6900eefd580bf08430c3cc9a9691671bf53d5
Israel <israelblenn@gmail.com>
2024-12-06 16:42:25 +1100

    troubleshooting

--- src/components/closenav.tsx
+++ src/components/closenav.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 
 export default function CloseNav() {
+
   useEffect(() => {
     const navLinks = document.querySelectorAll("nav ul li a");
     const checkbox = document.querySelector("label.hamburger input") as HTMLInputElement;
@@ -15,6 +16,8 @@ export default function CloseNav() {
     navLinks.forEach(link => {
       link.addEventListener("click", handleLinkClick);
     });
+
+    console.log("test - this runs in the browser!");
 
     return () => {
       navLinks.forEach(link => {
