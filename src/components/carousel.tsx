20c40069667bf22dd7be65e4f0cfd8126cdb8287
Israel <israelblenn@gmail.com>
2024-12-08 21:22:36 +1100

    troubleshooting

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -10,7 +10,7 @@ interface CarouselProps {
 }
 
 const FADE_DURATION = 1000
-const FADE_INTERVAL = 2500
+const FADE_INTERVAL = 1000
 
 const Carousel: React.FC<CarouselProps> = ({ media, alt }) => {
   const [currentIndex, setCurrentIndex] = useState(0)
